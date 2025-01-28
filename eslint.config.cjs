5e2c54b1ba55c4dd581af1568608f27563a2bbd5
Andreas Tennert <andreas@qupaya.com>
2025-01-28 04:27:34 +0100

    chore: fix eslint config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,10 +1,18 @@
 const nx = require('@nx/eslint-plugin');
-const importsAutoFix = require('eslint-plugin-sort-imports-es6-autofix');
-const unusedImports = require('eslint-plugin-unused-imports');
-const jest = require('eslint-plugin-jest');
-const importPlugin = require('eslint-plugin-import');
-const rxjs = require('eslint-plugin-rxjs');
-const rxjsAngular = require('eslint-plugin-rxjs-angular');
+const eslintPluginSortImportsEs6Autofix = require('eslint-plugin-sort-imports-es6-autofix');
+const eslintPluginUnusedImports = require('eslint-plugin-unused-imports');
+const eslintPluginJest = require('eslint-plugin-jest');
+const eslintPluginImport = require('eslint-plugin-import');
+const eslintPluginRxjs = require('eslint-plugin-rxjs');
+const eslintPluginRxjsAngular = require('eslint-plugin-rxjs-angular');
+
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
 
 module.exports = [
   ...nx.configs['flat/base'],
@@ -15,14 +23,13 @@ module.exports = [
   },
   {
     plugins: {
-      'sort-imports-es6-autofix': importsAutoFix,
-      'unused-imports': unusedImports,
-      import: importPlugin,
-      jest,
+      'sort-imports-es6-autofix': eslintPluginSortImportsEs6Autofix,
+      'unused-imports': eslintPluginUnusedImports,
+      import: eslintPluginImport,
+      jest: eslintPluginJest,
     },
   },
   {
-    name: '3',
     files: ['**/*.spec.ts'],
     rules: {
       'jest/consistent-test-it': 'error',
@@ -117,171 +124,178 @@ module.exports = [
       ],
     },
   },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    ...nx.configs['flat/typescript'],
-    ...rxjs.configs['recommended'],
-    plugins: {
-      rxjs,
-      'rxjs-angular': rxjsAngular,
-      import: importPlugin,
-    },
-    rules: {
-      'no-restricted-imports': [
-        'error',
-        {
-          name: '.',
-          message:
-            'Importing barrel files from the same or parent folder can lead to hard-to-find circular dependendies. Import the file directly.',
-        },
-        {
-          name: '..',
-          message:
-            'Importing barrel files from the same or parent folder can lead to hard-to-find circular dependendies. Import the file directly.',
-        },
-        {
-          name: '../..',
-          message:
-            'Importing barrel files from the same or parent folder can lead to hard-to-find circular dependendies. Import the file directly.',
-        },
-      ],
-      'no-restricted-globals': [
-        'error',
-        {
-          name: 'window',
-          message:
-            'Please use injected references:\n `@Inject(WINDOW) private readonly window: Window`',
-        },
-        {
-          name: 'document',
-          message:
-            'Please use injected references:\n `@Inject(DOCUMENT) private readonly document: Document`',
-        },
-      ],
-      eqeqeq: ['error', 'always'],
-      'no-await-in-loop': 'error',
-      'no-console': ['error'],
-      'no-constructor-return': 'error',
-      'no-promise-executor-return': 'error',
-      'no-duplicate-imports': [
-        'error',
-        {
-          includeExports: true,
-        },
-      ],
-      'no-self-compare': 'error',
-      'no-template-curly-in-string': 'error',
-      camelcase: 'warn',
-      curly: 'error',
-      'default-case': 'error',
-      'default-case-last': 'error',
-      'no-alert': 'error',
-      'no-var': 'error',
-      'prefer-const': 'error',
-      'prefer-template': 'error',
-      'sort-imports-es6-autofix/sort-imports-es6': [
-        2,
-        {
-          ignoreCase: false,
-          ignoreMemberSort: false,
-          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        },
-      ],
-      'unused-imports/no-unused-imports-ts': 2,
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/explicit-function-return-type': [
-        'error',
-        {
-          allowExpressions: true,
-        },
-      ],
-      '@typescript-eslint/no-floating-promises': ['error'],
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-        },
-      ],
-      '@typescript-eslint/prefer-readonly': 'error',
-      'rxjs/finnish': [
-        'error',
-        {
-          functions: true,
-          methods: true,
-          names: {
-            '^(canActivate|canActivateChild|canDeactivate|canLoad|intercept|resolve|validate)$': false,
+  ...compat
+    .config({
+      extends: ['plugin:@nx/typescript', 'plugin:rxjs/recommended'],
+    })
+    .map((config) => ({
+      ...config,
+      files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
+      plugins: {
+        rxjs: eslintPluginRxjs,
+        'rxjs-angular': eslintPluginRxjsAngular,
+        import: eslintPluginImport,
+      },
+      rules: {
+        ...config.rules,
+        'no-restricted-imports': [
+          'error',
+          {
+            name: '.',
+            message:
+              'Importing barrel files from the same or parent folder can lead to hard-to-find circular dependendies. Import the file directly.',
           },
-          parameters: true,
-          properties: true,
-          strict: false,
-          types: {
-            '^EventEmitter$': false,
+          {
+            name: '..',
+            message:
+              'Importing barrel files from the same or parent folder can lead to hard-to-find circular dependendies. Import the file directly.',
           },
-          variables: true,
-        },
-      ],
-      'rxjs/no-exposed-subjects': 'error',
-      'rxjs/no-unsafe-switchmap': 'error',
-      'rxjs/no-ignored-observable': 'error',
-      'rxjs/no-ignored-subscribe': 'error',
-      'rxjs/no-subject-value': 'error',
-      'rxjs-angular/prefer-takeuntil': [
-        'error',
-        {
-          alias: ['untilDestroyed'],
-          checkComplete: true,
-          checkDecorators: ['Component'],
-          checkDestroy: false,
-        },
-      ],
-      'rxjs/no-unsafe-takeuntil': [
-        'error',
-        {
-          alias: ['untilDestroyed'],
-        },
-      ],
-      'rxjs-angular/prefer-async-pipe': 'error',
-      '@typescript-eslint/no-extra-semi': 'error',
-      'no-extra-semi': 'off',
-    },
-  },
-  {
-    files: ['**/*.js', '**/*.jsx'],
-    // ...nx.configs['flat/javascript'],
-    rules: {
-      eqeqeq: ['error', 'always'],
-      'no-await-in-loop': 'error',
-      'no-constructor-return': 'error',
-      'no-promise-executor-return': 'error',
-      'no-duplicate-imports': [
-        'error',
-        {
-          includeExports: true,
-        },
-      ],
-      'no-self-compare': 'error',
-      'no-template-curly-in-string': 'error',
-      camelcase: 'warn',
-      curly: 'error',
-      'default-case': 'error',
-      'default-case-last': 'error',
-      'no-alert': 'error',
-      'no-var': 'error',
-      'prefer-const': 'error',
-      'prefer-template': 'error',
-      'sort-imports-es6-autofix/sort-imports-es6': [
-        2,
-        {
-          ignoreCase: false,
-          ignoreMemberSort: false,
-          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        },
-      ],
-      // 'unused-imports/no-unused-imports-ts': 2,
-      // '@typescript-eslint/no-extra-semi': 'error',
-      'no-extra-semi': 'off',
-    },
-  },
+          {
+            name: '../..',
+            message:
+              'Importing barrel files from the same or parent folder can lead to hard-to-find circular dependendies. Import the file directly.',
+          },
+        ],
+        'no-restricted-globals': [
+          'error',
+          {
+            name: 'window',
+            message:
+              'Please use injected references:\n `@Inject(WINDOW) private readonly window: Window`',
+          },
+          {
+            name: 'document',
+            message:
+              'Please use injected references:\n `@Inject(DOCUMENT) private readonly document: Document`',
+          },
+        ],
+        eqeqeq: ['error', 'always'],
+        'no-await-in-loop': 'error',
+        'no-console': ['error'],
+        'no-constructor-return': 'error',
+        'no-promise-executor-return': 'error',
+        'no-duplicate-imports': [
+          'error',
+          {
+            includeExports: true,
+          },
+        ],
+        'no-self-compare': 'error',
+        'no-template-curly-in-string': 'error',
+        camelcase: 'warn',
+        curly: 'error',
+        'default-case': 'error',
+        'default-case-last': 'error',
+        'no-alert': 'error',
+        'no-var': 'error',
+        'prefer-const': 'error',
+        'prefer-template': 'error',
+        'sort-imports-es6-autofix/sort-imports-es6': [
+          2,
+          {
+            ignoreCase: false,
+            ignoreMemberSort: false,
+            memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          },
+        ],
+        'unused-imports/no-unused-imports': 2,
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/explicit-function-return-type': [
+          'error',
+          {
+            allowExpressions: true,
+          },
+        ],
+        '@typescript-eslint/no-floating-promises': ['error'],
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            argsIgnorePattern: '^_',
+          },
+        ],
+        '@typescript-eslint/prefer-readonly': 'error',
+        'rxjs/finnish': [
+          'error',
+          {
+            functions: true,
+            methods: true,
+            names: {
+              '^(canActivate|canActivateChild|canDeactivate|canLoad|intercept|resolve|validate)$': false,
+            },
+            parameters: true,
+            properties: true,
+            strict: false,
+            types: {
+              '^EventEmitter$': false,
+            },
+            variables: true,
+          },
+        ],
+        'rxjs/no-exposed-subjects': 'error',
+        'rxjs/no-unsafe-switchmap': 'error',
+        'rxjs/no-ignored-observable': 'error',
+        'rxjs/no-ignored-subscribe': 'error',
+        'rxjs/no-subject-value': 'error',
+        'rxjs-angular/prefer-takeuntil': [
+          'error',
+          {
+            alias: ['untilDestroyed'],
+            checkComplete: true,
+            checkDecorators: ['Component'],
+            checkDestroy: false,
+          },
+        ],
+        'rxjs/no-unsafe-takeuntil': [
+          'error',
+          {
+            alias: ['untilDestroyed'],
+          },
+        ],
+        'rxjs-angular/prefer-async-pipe': 'error',
+        'no-extra-semi': 'off',
+      },
+    })),
+  ...compat
+    .config({
+      extends: ['plugin:@nx/javascript'],
+    })
+    .map((config) => ({
+      ...config,
+      files: ['**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
+      rules: {
+        ...config.rules,
+        eqeqeq: ['error', 'always'],
+        'no-await-in-loop': 'error',
+        'no-constructor-return': 'error',
+        'no-promise-executor-return': 'error',
+        'no-duplicate-imports': [
+          'error',
+          {
+            includeExports: true,
+          },
+        ],
+        'no-self-compare': 'error',
+        'no-template-curly-in-string': 'error',
+        camelcase: 'warn',
+        curly: 'error',
+        'default-case': 'error',
+        'default-case-last': 'error',
+        'no-alert': 'error',
+        'no-var': 'error',
+        'prefer-const': 'error',
+        'prefer-template': 'error',
+        'sort-imports-es6-autofix/sort-imports-es6': [
+          2,
+          {
+            ignoreCase: false,
+            ignoreMemberSort: false,
+            memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          },
+        ],
+        'unused-imports/no-unused-imports': 2,
+        'no-extra-semi': 'off',
+      },
+    })),
   {
     files: ['**/*.spec.ts', '**/test-setup.ts'],
     rules: {
